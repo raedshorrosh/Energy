d77755657a7735ae5f460d32a68d86dc9c33964b
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2023-05-05 14:29:53 +0300

    Update levels2.js

--- levels2.js
+++ levels2.js
@@ -12,6 +12,7 @@
 JXG.Options.layer.text = 0;
 
 // initialize mathJax
+/*
   MathJax.Hub.Config({
         extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js"],
         jax: ["input/TeX", "output/HTML-CSS"],
@@ -21,7 +22,7 @@ JXG.Options.layer.text = 0;
         },
         "HTML-CSS": { availableFonts: ["TeX"] }
       });
-
+*/
 
 var nameRef ={reacttxt:{#reactxt#}, prodtxt:{#prodtxt#}, p:["",""],chkd:false};
 
