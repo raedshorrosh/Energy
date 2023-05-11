a49f3aeaf23b24aa1fd8dc856e91a2e071832350
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2023-05-11 15:58:28 +0300

    Update levels3.js

--- levels3.js
+++ levels3.js
@@ -5,27 +5,10 @@
 [[jsxgraph width="600px" input-ref-ans1='ans1Ref'  input-ref-ans2='ans2Ref'  input-ref-ans3='ans3Ref' input-ref-ans4='ans4Ref'  input-ref-ans50='ans50Ref'  input-ref-ans51='ans51Ref'  input-ref-ans5='stateRef5'  input-ref-ans6='stateRef6'  input-ref-ans7='stateRef7']]
 
 var nameRef ={txttop:{#txttop#}, txtmid:{#txtmid#},txtbot:{#txtbot#}, p:["","","","","",""],chkd:false};
-
-  // boundingbox:[left, top, right, bottom]
-
-//snap grid for points in x
-//JXG.Options.point.snapToGrid = true;
-//JXG.Options.point.snapSizeX = {#snapx#};
 JXG.Options.layer.text = 0;
-// initialize mathJax
-/*MathJax.Hub.Config({
-        extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js"],
-        jax: ["input/TeX", "output/HTML-CSS"],
-        tex2jax: {
-          inlineMath: [ ['\(','\)'], ["\\(","\\)"] ],
-          displayMath: [ ['\[','\]'], ["\\[","\\]"] ],
-        },
-        "HTML-CSS": { availableFonts: ["TeX"] }
-      });
-*/
+
 //-----------------------------------------------------------------------
   var board = JXG.JSXGraph.initBoard(divid, {boundingbox: [-25, 10, 25, -10], axis: false, showCopyright: false,showNavigation:false,pan:{enabled:false},zoom:{enabled:false},resize: {enabled:false}});
-
 
 var xp={#Axis_p#},ylable_p={#ylable_p#};
 //lines top and bottom and hidden
