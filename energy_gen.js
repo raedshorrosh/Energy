d0edbe7b03253ccab3300d5c5867f796d15ae655
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2026-01-27 01:38:44 +0200

    Update energy_gen.js

--- energy_gen.js
+++ energy_gen.js
@@ -2,7 +2,7 @@
   input-ref-levelsRef='levelsRef' 
   input-ref-arrowsRef='arrowsRef' 
   input-ref-chemLabelsRef='chemLabelsRef']]
-//6.0
+//7.0
 var board = JXG.JSXGraph.initBoard(divid, {
     boundingbox: [-25, 15, 25, -15], 
     axis: false, 
@@ -31,7 +31,7 @@ var safeLoad = function(ref, def) {
 
 // 2. Enthalpy Axis
 board.create('arrow', [[xp, -14], [xp, 14]], {strokeColor: 'black', strokeWidth: 2});
-board.create('text', [xp - 1.5, 0, "{@enthalpy_label@}"], {rotate: 90, fontSize: 18, fixed: true});
+board.create('text', [xp - 1.5, 0, "{#enthalpy_label#}"], {rotate: 90, fontSize: 18, fixed: true});
 
 // 3. Levels
 var levelPoints = [];
@@ -41,7 +41,7 @@ var levelX = xp + 1;
 for (var i = 0; i < labels.length; i++) {
     (function(idx) {
         var p = board.create('point', [levelX, currentLevelsY[idx]], {
-            name: '', fixed: isFixed[idx] == 1, size: 4, color: 'blue', strokeColor: 'black', showInfobox: false
+            name: '', fixed: isFixed[idx] == 1, size: 4, color: 'blue', strokeColor: 'black', showInfobox: false, moveAlongX: false
         });
         var seg = board.create('segment', [p, [function(){ return p.X() + len; }, function(){ return p.Y(); }]], { 
             strokeColor: 'black', strokeWidth: 3 
@@ -63,29 +63,29 @@ var currentArrows = safeLoad(arrowsRef, defaultArrows);
 
 for (var j = 0; j < arrLabels.length; j++) {
     (function(idx) {
-        var p1 = board.create('point', currentArrows[idx][0], {name: '', color: colors[idx % 3], size: 4, face: '[]', showInfobox: false});
-        var p2 = board.create('point', currentArrows[idx][1], {name: '', color: colors[idx % 3], size: 2, showInfobox: false});
+        var p1 = board.create('point', currentArrows[idx][0], {name: '', color: colors[idx % 3], size: 4, face: '[]', showInfobox: false, moveAlongX: false});
+        var p2 = board.create('point', currentArrows[idx][1], {name: '', color: colors[idx % 3], size: 2, showInfobox: false, moveAlongX: false});
         var seg = board.create('segment', [p1, p2], {strokeColor: colors[idx % 3], strokeWidth: 3, lastarrow: {type: 2, size: 6}});
         board.create('text', [function(){ return (p1.X() + p2.X()) / 2 + 0.5; }, function(){ return (p1.Y() + p2.Y()) / 2; }, arrLabels[idx]], { color: colors[idx % 3], useMathJax: true, fontSize: 14 });
         arrows.push({p1: p1, p2: p2, seg: seg, origX: arrowX});
     })(j);
 }
 
-// 5. ATTRACORS AND STRICT HORIZONTAL LOCK
+// 5. ATTRACORS, SYNC & STRICT HORIZONTAL LOCK
 board.on('move', function() {
-    // Force Levels back to their X column
+    // 1. Force Levels to stay on their X column
     for (var a = 0; a < levelPoints.length; a++) {
         levelPoints[a].p.moveTo([levelPoints[a].origX, levelPoints[a].p.Y()]);
     }
 
-    // Force Arrows back to their X column + Snapping to Levels
+    // 2. Force Arrows to stay on their X column AND Follow Levels
     for (var b = 0; b < arrows.length; b++) {
         var pts = [arrows[b].p1, arrows[b].p2];
         for (var pIdx = 0; pIdx < pts.length; pIdx++) {
             var p = pts[pIdx];
             var targetY = p.Y();
             
-            // Snapping: if point Y is within 0.6 units of any level line Y
+            // Snapping & Following logic: if point Y is close to a level, latch it
             for (var l = 0; l < levelPoints.length; l++) {
                 if (Math.abs(p.Y() - levelPoints[l].p.Y()) < 0.6) {
                     targetY = levelPoints[l].p.Y();
@@ -115,14 +115,9 @@ if (feedbackEl) {
 }
 
 var updateInputs = function() {
-    var lvlsData = levelPoints.map(function(obj) { return obj.p.Y(); });
-    document.getElementById(levelsRef).value = JSON.stringify(lvlsData);
-
-    var arrsData = arrows.map(function(obj) { return [[obj.p1.X(), obj.p1.Y()], [obj.p2.X(), obj.p2.Y()]]; });
-    document.getElementById(arrowsRef).value = JSON.stringify(arrsData);
-
-    var chemsData = levelPoints.map(function(obj) { return [obj.p.X() + 2, obj.p.Y() + 0.6]; });
-    document.getElementById(chemLabelsRef).value = JSON.stringify(chemsData);
+    document.getElementById(levelsRef).value = JSON.stringify(levelPoints.map(function(obj) { return obj.p.Y(); }));
+    document.getElementById(arrowsRef).value = JSON.stringify(arrows.map(function(obj) { return [[obj.p1.X(), obj.p1.Y()], [obj.p2.X(), obj.p2.Y()]]; }));
+    document.getElementById(chemLabelsRef).value = JSON.stringify(levelPoints.map(function(obj) { return [obj.p.X() + 2, obj.p.Y() + 0.6]; }));
     
     [levelsRef, arrowsRef, chemLabelsRef].forEach(function(id) {
         var el = document.getElementById(id);
