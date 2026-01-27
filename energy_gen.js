5b00dede1af7108a87a8b999c288c3df924de15c
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2026-01-27 15:59:29 +0200

    Update energy_gen.js

--- energy_gen.js
+++ energy_gen.js
@@ -2,7 +2,7 @@
   input-ref-levelsRef='levelsRef' 
   input-ref-arrowsRef='arrowsRef' 
   input-ref-chemLabelsRef='chemLabelsRef']]
-
+//12.0
 var board = JXG.JSXGraph.initBoard(divid, {
     boundingbox: [-25, 15, 25, -15], 
     axis: false, 
@@ -45,6 +45,11 @@ for (var i = 0; i < labels.length; i++) {
             name: '', fixed: isFixed[idx] == 1, size: 4, color: 'blue', strokeColor: 'black', showInfobox: false
         });
         
+        // Strict horizontal lock for energy levels
+        p.on('drag', function() {
+            p.moveTo([levelX, p.Y()]);
+        });
+
         var seg = board.create('segment', [p, [function(){ return p.X() + len; }, function(){ return p.Y(); }]], { 
             strokeColor: 'black', strokeWidth: 3 
         });
@@ -67,7 +72,6 @@ var currentArrows = safeLoad(arrowsRef, defaultArrows);
 
 for (var j = 0; j < arrLabels.length; j++) {
     (function(idx) {
-        // We use the segment of each level as an attractor
         var attractorsList = levelPoints.map(function(lp) { return lp.seg; });
 
         var p1 = board.create('point', currentArrows[idx][0], {
@@ -84,6 +88,10 @@ for (var j = 0; j < arrLabels.length; j++) {
             snatchDistance: 1.0
         });
 
+        // Strict horizontal lock for arrow points
+        p1.on('drag', function() { p1.moveTo([arrowX, p1.Y()]); });
+        p2.on('drag', function() { p2.moveTo([arrowX, p2.Y()]); });
+
         var seg = board.create('segment', [p1, p2], {strokeColor: colors[idx % 3], strokeWidth: 3, lastarrow: {type: 2, size: 6}});
         
         board.create('text', [function(){ return (p1.X() + p2.X()) / 2 + 0.5; }, function(){ return (p1.Y() + p2.Y()) / 2; }, arrLabels[idx]], { color: colors[idx % 3], useMathJax: true, fontSize: 14 });
@@ -92,20 +100,7 @@ for (var j = 0; j < arrLabels.length; j++) {
     })(j);
 }
 
-// 5. GLOBAL MOVE HANDLER (Ensuring strict X-alignment)
-board.on('move', function() {
-    // Keep Levels on their X line
-    for (var a = 0; a < levelPoints.length; a++) {
-        levelPoints[a].p.moveTo([levelPoints[a].x, levelPoints[a].p.Y()]);
-    }
-    // Keep Arrows on their X line
-    for (var b = 0; b < arrows.length; b++) {
-        arrows[b].p1.moveTo([arrows[b].x, arrows[b].p1.Y()]);
-        arrows[b].p2.moveTo([arrows[b].x, arrows[b].p2.Y()]);
-    }
-});
-
-// 6. Visual Feedback Logic
+// 5. Visual Feedback Logic
 var feedbackEl = document.getElementById(rqm);
 if (feedbackEl) {
     try {
@@ -123,7 +118,7 @@ if (feedbackEl) {
     } catch(e) {}
 }
 
-// 7. Data Sync
+// 6. Data Sync
 var updateInputs = function() {
     document.getElementById(levelsRef).value = JSON.stringify(levelPoints.map(function(obj) { return obj.p.Y(); }));
     document.getElementById(arrowsRef).value = JSON.stringify(arrows.map(function(obj) { return [[obj.p1.X(), obj.p1.Y()], [obj.p2.X(), obj.p2.Y()]]; }));
