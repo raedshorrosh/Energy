fa2b70d51798764a040b1a87298f315507fbca5f
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2026-01-27 15:55:35 +0200

    Update energy_gen.js

--- energy_gen.js
+++ energy_gen.js
@@ -2,7 +2,7 @@
   input-ref-levelsRef='levelsRef' 
   input-ref-arrowsRef='arrowsRef' 
   input-ref-chemLabelsRef='chemLabelsRef']]
-//11.0
+
 var board = JXG.JSXGraph.initBoard(divid, {
     boundingbox: [-25, 15, 25, -15], 
     axis: false, 
@@ -12,12 +12,11 @@ var board = JXG.JSXGraph.initBoard(divid, {
     zoom: {enabled: false}
 });
 
-// 1. Config from Maxima (Using safer substitution wrappers)
+// 1. Config from Maxima
 var xp = Number("{#Axis_p#}") || -5;
 var len = Number("{#l_length#}") || 25;
 var rqm = "{#rqm#}";
 
-// Safely parse Maxima lists/arrays
 var isFixed = {#levels_fixed#};
 var startY = {#levels_y_init#};
 var labels = {#levels_txt#};
@@ -46,16 +45,13 @@ for (var i = 0; i < labels.length; i++) {
             name: '', fixed: isFixed[idx] == 1, size: 4, color: 'blue', strokeColor: 'black', showInfobox: false
         });
         
-        // Horizontal Lock for Level Points
-        p.on('drag', function() { p.moveTo([levelX, p.Y()]); });
-
         var seg = board.create('segment', [p, [function(){ return p.X() + len; }, function(){ return p.Y(); }]], { 
             strokeColor: 'black', strokeWidth: 3 
         });
         
         board.create('text', [function(){ return p.X() + 2; }, function(){ return p.Y() + 0.6; }, labels[idx]], { useMathJax: true, fontSize: 14, fixed: true });
         
-        levelPoints.push({p: p, seg: seg, origX: levelX});
+        levelPoints.push({p: p, seg: seg, x: levelX});
     })(i);
 }
 
@@ -71,34 +67,45 @@ var currentArrows = safeLoad(arrowsRef, defaultArrows);
 
 for (var j = 0; j < arrLabels.length; j++) {
     (function(idx) {
-        // Create Arrow Points with Native Attractors (Snapping to Levels)
+        // We use the segment of each level as an attractor
+        var attractorsList = levelPoints.map(function(lp) { return lp.seg; });
+
         var p1 = board.create('point', currentArrows[idx][0], {
             name: '', color: colors[idx % 3], size: 4, face: '[]', showInfobox: false,
-            attractors: levelPoints.map(function(lp){ return lp.seg; }),
-            attractorDistance: 0.6,
-            snatchDistance: 0.8
+            attractors: attractorsList,
+            attractorDistance: 0.8,
+            snatchDistance: 1.0
         });
         
         var p2 = board.create('point', currentArrows[idx][1], {
             name: '', color: colors[idx % 3], size: 2, showInfobox: false,
-            attractors: levelPoints.map(function(lp){ return lp.seg; }),
-            attractorDistance: 0.6,
-            snatchDistance: 0.8
+            attractors: attractorsList,
+            attractorDistance: 0.8,
+            snatchDistance: 1.0
         });
-
-        // Horizontal Lock for Arrow Points
-        p1.on('drag', function() { p1.moveTo([arrowX, p1.Y()]); });
-        p2.on('drag', function() { p2.moveTo([arrowX, p2.Y()]); });
 
         var seg = board.create('segment', [p1, p2], {strokeColor: colors[idx % 3], strokeWidth: 3, lastarrow: {type: 2, size: 6}});
         
         board.create('text', [function(){ return (p1.X() + p2.X()) / 2 + 0.5; }, function(){ return (p1.Y() + p2.Y()) / 2; }, arrLabels[idx]], { color: colors[idx % 3], useMathJax: true, fontSize: 14 });
         
-        arrows.push({p1: p1, p2: p2, seg: seg, origX: arrowX});
+        arrows.push({p1: p1, p2: p2, seg: seg, x: arrowX});
     })(j);
 }
 
-// 5. Visual Feedback Logic (Grading Colors)
+// 5. GLOBAL MOVE HANDLER (Ensuring strict X-alignment)
+board.on('move', function() {
+    // Keep Levels on their X line
+    for (var a = 0; a < levelPoints.length; a++) {
+        levelPoints[a].p.moveTo([levelPoints[a].x, levelPoints[a].p.Y()]);
+    }
+    // Keep Arrows on their X line
+    for (var b = 0; b < arrows.length; b++) {
+        arrows[b].p1.moveTo([arrows[b].x, arrows[b].p1.Y()]);
+        arrows[b].p2.moveTo([arrows[b].x, arrows[b].p2.Y()]);
+    }
+});
+
+// 6. Visual Feedback Logic
 var feedbackEl = document.getElementById(rqm);
 if (feedbackEl) {
     try {
@@ -116,7 +123,7 @@ if (feedbackEl) {
     } catch(e) {}
 }
 
-// 6. Data Synchronization
+// 7. Data Sync
 var updateInputs = function() {
     document.getElementById(levelsRef).value = JSON.stringify(levelPoints.map(function(obj) { return obj.p.Y(); }));
     document.getElementById(arrowsRef).value = JSON.stringify(arrows.map(function(obj) { return [[obj.p1.X(), obj.p1.Y()], [obj.p2.X(), obj.p2.Y()]]; }));
